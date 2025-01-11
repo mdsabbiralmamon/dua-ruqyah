4f0545c2ca2ae95cd472850d1dd9ba9d9a235e71
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-11 18:34:47 +0600

    add: create main page component with NavbarMain

---
+++ src/app/duas/page.tsx
@@ -0,0 +1,12 @@
+import NavbarMain from '@/components/shared/NavbarMain/NavbarMain';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <NavbarMain />
+        </div>
+    );
+};
+
+export default page;
