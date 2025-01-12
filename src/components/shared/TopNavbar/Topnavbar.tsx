8689ef910418acc2d3c5807757c2fae8b9fae146
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-12 15:15:32 +0600

    add: integrate ProfileAvatar component into Topnavbar for enhanced user navigation

--- src/components/shared/TopNavbar/Topnavbar.tsx
+++ src/components/shared/TopNavbar/Topnavbar.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import Search from '../Search/Search';
+import React from "react";
+import Search from "../Search/Search";
+import ProfileAvatar from "../ProfileAvatar/ProfileAvatar";
 
 const Topnavbar = () => {
-    return (
-        <div className='w-full h-16 flex items-center justify-between'>
-           <h1 className='text-2xl font-semibold'>Dua Page</h1>
-           {/* search bar */}
-           <Search />
-        </div>
-    );
+  return (
+    <div className="w-full h-16 flex items-center justify-between">
+      <h1 className="text-2xl font-semibold">Dua Page</h1>
+      <div className="flex items-center space-x-64">
+        {/* search bar */}
+        <Search />
+        {/* Avatar / Mini Navigation */}
+        <ProfileAvatar />
+      </div>
+    </div>
+  );
 };
 
 export default Topnavbar;
