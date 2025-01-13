57199466534cfc7d687ec62d3f9e13153589f295
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-13 23:07:35 +0600

    fix: update Page component to manage selected subcategory state and pass it to MainContent

--- src/app/duas/page.tsx
+++ src/app/duas/page.tsx
@@ -1,20 +1,24 @@
+'use client';
+
 import Categories from "@/components/DuaPage/Categories/Categories";
 import MainContent from "@/components/DuaPage/MainContent/MainContent";
 import NavbarMain from "@/components/shared/NavbarMain/NavbarMain";
 import RightNavbar from "@/components/shared/RightNavbar/RightNavbar";
 import Topnavbar from "@/components/shared/TopNavbar/Topnavbar";
-import React from "react";
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+
   return (
-    <div>
+    <div className="">
       <div className="flex gap-8">
         <NavbarMain />
         <div className="w-full">
           <Topnavbar />
           <div className="flex justify-between my-4 gap-5">
-            <Categories />
-            <MainContent />
+            <Categories onSubcategoryClick={setSelectedSubcategory} />
+            <MainContent subcategory={selectedSubcategory} />
             <RightNavbar />
           </div>
         </div>
@@ -23,4 +27,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
