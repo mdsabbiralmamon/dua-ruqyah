6618dea0deb474cdff20d894e992564bb78c1197
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-13 19:37:16 +0600

    add: create SectionHead component for displaying section titles in Dua page

---
+++ src/components/DuaPage/SectionHead/SectionHead.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionHead = () => {
+    return (
+        <div className='w-full bg-white p-4 shadow-lg rounded-xl'>
+            <h2 className='font-semibold'> <span className='text-emerald-500'>Section: </span>This is section title</h2>
+        </div>
+    );
+};
+
+export default SectionHead;
