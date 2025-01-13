1fa03893fd53a6eb3d327136cb9c4c1deb254347
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2025-01-13 23:07:52 +0600

    fix: remove unnecessary class from SectionHead component's title

--- src/components/DuaPage/SectionHead/SectionHead.tsx
+++ src/components/DuaPage/SectionHead/SectionHead.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionHead = () => {
     return (
         <div className='w-full bg-white p-4 shadow-lg rounded-xl'>
-            <h2 className='font-semibold'> <span className='text-emerald-500'>Section: </span>This is section title</h2>
+            <h2 className=''> <span className='text-emerald-500'>Section: </span>This is section title</h2>
         </div>
     );
 };
